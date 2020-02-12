1c19c1c4357288fb2fd144fe7d8e41c305a59a4c
jaa1817 <jaa1817@skbl-422-013.ph.ic.ac.uk>
2020-02-12 18:29:53 +0000

    Output messages

--- Tamagotchi.asm
+++ Tamagotchi.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    extern LCD_Setup, LCD_Send_Byte_D,LCD_shift
+    extern LCD_Setup, LCD_Send_Byte_D,LCD_shift, LCD_clear
 	
 acs0 udata_acs
 counter_row res 1
@@ -16,32 +16,67 @@ myArray res 0x80
  
 pdata code
 
-Line1	data "PRESS A TO START"
-        variable Line_Bytes = .16
-Line2	data "PRESS B FORDEATH"     
+Line1	data "ABC"
+        variable Line_Bytes = .3
+Line2   data "DEF" 
+Line3   data 0x30
+   
 	
 	
-tamagotchi code 
+tamagotchi code
  
-rst	code	0    ; reset vector
+	org 0x0
 	goto	setup
+	org 0x100
+ 
+;rst	code	0    ; reset vector
 setup	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
+	nop
+	nop
+	nop 
 	call	LCD_Setup	; setup LCD
-	
- 
+	goto main
+
+
 main    
-	#define Line Line1
-	bra output
-	#undefine Line
-	#define Line Line2
-	bra output
-	
-	
+	# Line = Line1
+	call output
+	;#undefine Line
+	;movlw b'11000000'
+	;call LCD_shift
+	;#define Line Line2
+	;call output
+	;movlw 0x00
+	;movwf Line
+	;#undefine Line
+	;movlw b'11001000'
+	;call LCD_shift
+	;movlw 0x30
+	;call LCD_Send_Byte_D
+	;movlw b'10001101'
+	;call LCD_shift
+	;movlw 0x30
+	;call LCD_Send_Byte_D
+	;movlw 0x30
+	;call LCD_Send_Byte_D
+	;movlw 0x30
+	;call LCD_Send_Byte_D
+	;movlw b'11001101'
+	;call LCD_shift
+	;movlw 0x30
+	;call LCD_Send_Byte_D
+	;movlw 0x30
+	;call LCD_Send_Byte_D
+	;movlw 0x30
+	;call LCD_Send_Byte_D
+	;;#define Line b'11100010'
+	;#define Line_Bytes .1
+	;bra output
+	bra finished
 	
 	
 
- 
 output  movlw	upper(Line)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(Line)	; address of data in PM
@@ -55,12 +90,21 @@ loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	call    LCD_Send_Byte_D
 	decfsz	counter_output	; count down to zero
 	bra	loop		; keep going until finished
-
-	goto	$		; goto current line in code
+	return  		; goto current line in code
 	
-	
-delay	decfsz	delay_count	; decrement until zero
-	bra delay
+delay
+	movlw 0xFF
+	movwf delay_count
+d	decfsz	delay_count	; decrement until zero
+	bra d
 	return
 
- end
+	
+
+finished
+	nop 
+	nop 
+	nop 
+	nop
+	
+	end
--- build/default/production/Tamagotchi.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/Tamagotchi.o:  \
-	H:/Tamagotchi/Tamagotchi.X/Tamagotchi.asm  \
-
